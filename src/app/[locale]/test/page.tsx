96a205401f435e5b9dddc656047a9a18779cd1af
Kyberkant <knight4@post.cz>
2026-05-19 10:22:51 +0200

    testovaní stránky

---
+++ src/app/[locale]/test/page.tsx
@@ -0,0 +1,20 @@
+import { Button, Stack, Text } from "@mantine/core";
+
+export default function Page() {
+  return (
+    <Stack>
+      <Text>Test stránka</Text>
+
+      <Button>Ahoj</Button>
+
+      <Button>Druhý button</Button>
+    </Stack>
+  );
+}
+
+
+
+// export default function Page() {
+//   //test
+// }
+
