4ec3a3560fe8f7279d0271c6b0166afe37e1534b
Kyberkant <knight4@post.cz>
2026-05-19 10:42:16 +0200

    vytvořena stránka na přehled inzerátů

---
+++ src/app/[locale]/prehled-inzeratu/page.tsx
@@ -0,0 +1,13 @@
+import { Button, Stack, Text } from "@mantine/core";
+
+export default function Page() {
+  return (
+    <Stack>
+      <Text>Test stránka</Text>
+
+      <Button>Ahoj</Button>
+
+      <Button>Druhý button</Button>
+    </Stack>
+  );
+}
